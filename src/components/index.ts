9a66309b8c346f8021f7b26595abfd541c98d131
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2022-06-07 22:19:54 +0300

    Stable extraction
    Signed-off-by: Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,10 @@
-export { default as Button } from "./Button";
+import Button from './Button';
+import HappiGraph from './happi-graph.component';
+import HappiGraphActions from './happi-graph-actions.component';
+
+export {
+  Button,
+  HappiGraph,
+  HappiGraphActions
+}
+
